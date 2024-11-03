4c2995924ed71dd689619ee203640f00612ce564
yynil <yueyu.lin@me.com>
2024-11-03 22:46:56 +0800

    更新导入路径，注释掉不必要的代码段以简化聊天函数

--- gradio/chat_rwkv7.py
+++ gradio/chat_rwkv7.py
@@ -12,7 +12,7 @@ import torch
 import yaml
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from rwkv_llama.utilities import HybridCache
-from rwkv_llama.hybrid_model_run import create_rwkv_args, HybridModel
+from rwkv_llama.hybrid_model_run_rwkv7 import create_rwkv_args, HybridModel
 
 
 
@@ -64,9 +64,9 @@ def chat(message, history, session):
     print(session["conversation"])
     current_input_text = tokenizer.apply_chat_template(session["conversation"], tokenize=False, add_generation_prompt=True)
     print(current_input_text)
-    index_of_im_start = current_input_text.find("<|im_start|>user")
-    if index_of_im_start != -1:
-        current_input_text = current_input_text[index_of_im_start:]
+    # index_of_im_start = current_input_text.find("<|im_start|>user")
+    # if index_of_im_start != -1:
+    #     current_input_text = current_input_text[index_of_im_start:]
     print(current_input_text)
     input_ids = tokenizer(current_input_text, return_tensors="pt").to("cuda:0")
     input_length = input_ids.input_ids.shape[1]
